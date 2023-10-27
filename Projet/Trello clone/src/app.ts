376f9406425b79403782ad20d6a331d4b671f169
David Andrianarivo <andrianarivo.david@gmail.com>
2023-10-27 18:28:15 +0300

    Detruire un element

---
+++ Projet/Trello clone/src/app.ts
@@ -0,0 +1,21 @@
+const itemsContainer = document.querySelectorAll('.items-container') as NodeListOf<HTMLDivElement>;
+
+function deleteBtnListeners(btn: HTMLButtonElement) {
+  btn.addEventListener('click', handleContainerDeletion)
+}
+
+function handleContainerDeletion(e: MouseEvent) {
+  const btn = e.target as HTMLButtonElement
+  const btnsArray = [...document.querySelectorAll('.delete-container-btn')] as HTMLButtonElement[]
+  const containers = [...document.querySelectorAll('.items-container')] as HTMLDivElement[]
+  containers[btnsArray.indexOf(btn)].remove()
+}
+
+function addContainerListeners(currentContainer: HTMLDivElement) {
+  const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
+  deleteBtnListeners(currentContainerDeletionBtn)
+}
+
+itemsContainer.forEach((container) => {
+  addContainerListeners(container)
+});
